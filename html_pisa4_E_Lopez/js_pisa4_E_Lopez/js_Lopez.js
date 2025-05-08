d44cbc2bf4218dbccb9b4fbfc9e90d2286e2a5b8
S.Lopez <sergiolf03@gmail.com>
2025-05-08 11:15:54 +0200

    Modificacion Plantilla

---
+++ html_pisa4_E_Lopez/js_pisa4_E_Lopez/js_Lopez.js
@@ -0,0 +1,31 @@
+// Índice dinámico: Añade aquí los enlaces a tus capítulos creados
+document.addEventListener("DOMContentLoaded", () => {
+  const indice = document.getElementById("indice-dinamico");
+
+  const capitulos = [
+    {
+      titulo:
+        "Economía verde y circular aplicada a en nuestro sector productivo",
+      archivo: "/html_pisa4_E_Lopez/2_ra5_economia_verde_circular_Lopez.html",
+    },
+    {
+      titulo: "Estrategias sostenibles  en nuestro sector productivo",
+      archivo: "/html_pisa4_E_Lopez/5_ra5_economia_verde_circular_Lopez.html",
+    },
+    {
+      titulo: "Normativa ambiental aplicable nuestro sector productivo",
+      archivo: "/html_pisa4_E_Lopez/8_ra5_economia_verde_circular_Lopez.html",
+    },
+    {
+      titulo: "Estrategias sostenibles en empresas TIC",
+      archivo: "/html_pisa4_E_Lopez/11_ra5_economia_verde_circular_Lopez.html",
+    },
+  ];
+
+  capitulos.forEach((capitulo) => {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerHTML = `<a href="${capitulo.archivo}" target="_blank">${capitulo.titulo} </a>`;
+    indice.appendChild(li);
+  });
+});
